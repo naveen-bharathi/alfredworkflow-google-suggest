74d648a78916d2c3a0fcfdfdf256bc8fd21d5aca
Naveen Bharathi <naveen@naveenbharathi.com>
2021-08-22 23:21:07 +0530

    added v1.0.0

---
+++ README.md
@@ -0,0 +1,50 @@
+# alfredworkflow-google-suggest
+
+Alfred workflow to list suggestions for Google Search.
+
+![Alfred google suggest screenshot](screenshots/screenshot.png)
+
+
+
+## Prerequisites
+
+- The [deno](https://deno.land) runtime should have been installed
+
+
+
+## How to use
+
+1. Open Alfred App
+2. Type `g `
+3. Type a query to search
+    - Example: `g what is the weather`
+4. Wait for results
+5. Then:
+    - `Return` to search on Google
+    - `cmd + c` to copy the link.
+
+
+
+# License
+
+MIT License
+
+Copyright (c) 2021 Naveen Bharathi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.ts
@@ -0,0 +1,90 @@
+import { parse } from 'https://deno.land/x/xml/mod.ts';
+
+type TResult = {
+  toplevel: {
+    CompleteSuggestion?: {
+      suggestion: {
+        '@data': string;
+      };
+    }[];
+  };
+}
+
+type TAlfredResultProps = {
+  arg?: string;
+  copy?: string;
+  icon?: string;
+  largetype?: string;
+  subtitle?: string;
+  title: string;
+  valid?: boolean;
+}
+
+const getGoogleSearchUrl = (query: string): string => (
+  `https://www.google.com/search?q=${query}`
+);
+
+const getGoogleSuggestionsApi = (query: string): string => (
+  `https://suggestqueries.google.com/complete/search?output=toolbar&q=${query}`
+);
+
+
+const getAlfredResult = (props: TAlfredResultProps) => ({
+  title: props.title,
+  ...props?.subtitle && {
+    subtitle: props?.subtitle,
+  },
+  ...props?.arg && {
+    arg: props?.arg,
+  },
+  icon: {
+    path: props?.icon || 'icons/google.png',
+  },
+  text: {
+    copy: props?.copy || props?.arg || '',
+    largetype: props?.largetype || props?.arg || '',
+  },
+  valid: props?.valid,
+});
+
+try {
+  const query = Deno.args[0] || '';
+
+  const res = await fetch(getGoogleSuggestionsApi(query));
+  const body: TResult = await parse(await res.text()) as TResult;
+
+  const packages = {
+    items: [
+      ...(body.toplevel?.CompleteSuggestion?.length
+        ? [
+          ...body.toplevel.CompleteSuggestion.map((item) => (
+            getAlfredResult({
+              title: item.suggestion['@data'],
+              arg: getGoogleSearchUrl(item.suggestion['@data']),
+            })
+          )),
+        ]
+        : [
+          getAlfredResult({
+            title: `No suggestions were found for "${query}"`,
+            valid: false,
+          }),
+        ]),
+    ],
+  };
+
+  console.log(JSON.stringify(packages));
+} catch (error) {
+  console.log(
+    JSON.stringify({
+      items: [
+        getAlfredResult({
+          title: error.name,
+          subtitle: error.message,
+          copy: error.stack,
+          valid: false,
+        }),
+      ],
+    }),
+  );
+}
